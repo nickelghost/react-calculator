d78b923754d70eba7b1014d42f80434ab22062c9
Gabriel Karczewski <nickelghost@tuta.io>
2018-04-04 12:16:29 +0100

    restricted input on the calculator

--- src/Calculator/Calculator.js
+++ src/Calculator/Calculator.js
@@ -3,10 +3,20 @@ import React, { Component } from 'react';
 import './Calculator.css';
 
 class Calculator extends Component {
+  getAllowedKeys = () => [
+    '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '*', '/', 'Enter'
+  ]
+
+  handleKeyPress = (e) => {
+    if (!this.getAllowedKeys().includes(e.key)) {
+      e.preventDefault();
+    }
+  }
+
   render() {
     return (
       <div className="Calculator">
-        <input className="Calculator-input" />
+        <input className="Calculator-input" onKeyPress={this.handleKeyPress} />
       </div>
     );
   }
